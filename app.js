f7489409db163d7cb58752718f434487739da769
Victor Villazon <victorzn23@gmail.com>
2023-09-26 21:36:33 -0500

    consultando los productos de dummyjson

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // alert('Hola G15!')
-// console.log('Hola G15!')
+console.log('Hola G15!')
+
+const url = 'https://dummyjson.com/products'
+
+async function fetchProducts() {
+  const response = await fetch(url) // Devuelve una Response
+
+  const data = await response.json() // Devuelve una promise
+  
+  console.log(data)
+}
+
+fetchProducts()
